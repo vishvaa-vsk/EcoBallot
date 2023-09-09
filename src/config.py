bff281d918d3dbe1169028ef41144df61975b01c
vishvaa-vsk <kvishvaa6@gmail.com>
2023-09-09 17:40:19 +0530

    updated config.py

--- src/config.py
+++ src/config.py
@@ -17,7 +17,7 @@ load_dotenv(path.join(basedir,".env"))
 class Config:
     SECRET_KEY = environ.get('SECRET_KEY')
     # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://username:password@localhost/database_name"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///eElection.sqlite3"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///EcoBallot.sqlite3"
     UPLOAD_FOLDER ='static/img'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
